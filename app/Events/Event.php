bcd7242de24f72e2561df865601a547096f96f70
Cali Castle <cali@calicastle.com>
2016-06-16 12:49:56 +0800

    Install Laravel

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Createdu\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Createdu\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
